6c0f696f086ab7604a05c2a212973c0b7f16c958
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-05-31 22:04:59 +0530

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rohitconsoleapp
+{
+   public class Class1
+    {
+
+        public void Show()
+        {
+
+            Console.WriteLine("Datatype HandsOn");
+
+            int digit = 20;
+            float FNumber = 2.2F;
+            double DoubleNumber = 5.99D;
+            long LNumber = 152663646467;
+            char Letter = 'D';
+            bool Bool = true;
+            string stringText = "Hello";
+            Console.WriteLine(digit);
+            Console.WriteLine(FNumber);
+            Console.WriteLine(DoubleNumber);
+            Console.WriteLine(LNumber);
+            Console.WriteLine(Letter);
+            Console.WriteLine(Bool);
+            Console.WriteLine(stringText);
+
+            Console.ReadKey();
+        }
+    }
+}
