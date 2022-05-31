cc28317195e99c0ce91a0eb0331879d496a49e14
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-05-31 22:15:45 +0530

    Add files via upload

---
+++ LoopClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rohitconsoleapp
+{
+    internal class LoopClass
+    {
+        public void Show()
+        {
+            Console.WriteLine("For loop");
+            for (int k = 0; k <= 10; k = k+2)
+            {
+                Console.WriteLine(k +"  Forloop");
+            }
+            Console.WriteLine("While loop");
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i + "  whileloop");
+                i++;
+            }
+            Console.WriteLine("do while loop");
+            int j = 0;
+            do
+            {
+                Console.WriteLine(j + "  doWhileloop");
+                j++;
+            }
+            while (j < 5);
+
+            Console.ReadKey();
+        }
+    }
+}
