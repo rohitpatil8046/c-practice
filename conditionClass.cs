47f591913afa387cad1ede19786395e8351eacd7
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-05-31 22:30:51 +0530

    Add files via upload

---
+++ conditionClass.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rohitconsoleapp
+{
+    internal class conditionClass
+    {
+        public void OutPutMethod()
+        {
+            int time = 20;
+            if (time < 18)
+            {
+                Console.WriteLine("Good day.");
+                if (time < 10)
+                {
+                    Console.WriteLine("Morning Time");
+                    if(time < 8)
+                    {
+                        Console.WriteLine("Good Morning");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Morning");
+                    }
+                }
+                else
+                {
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Good evening.");
+            }
+            Console.ReadKey();
+        }
+        
+
+    }
+}
