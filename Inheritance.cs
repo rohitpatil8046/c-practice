036f772d42c5bbb2b9e3ada4aff4e6dfd78d5005
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-01 19:14:01 +0530

    Add files via upload

---
+++ Inheritance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rohitconsoleapp
+{
+    internal class Inheritance
+    {
+        // base class
+        class Animal
+        {
+            protected void eat()
+            {
+                Console.WriteLine("I can eat");
+            }
+        }
+
+        // derived class of Animal 
+        class Dog : Animal
+        {
+            static void Main(string[] args)
+            {
+
+                Dog labrador = new Dog();
+
+                // access protected method from base class
+                labrador.eat();
+
+                Console.ReadLine();
+            }
+        }
+    }
+}
