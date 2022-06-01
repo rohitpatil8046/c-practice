d0997e461ad5fb9d59bfaa6bac6db13ecd6a1f9c
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-01 15:13:17 +0530

    Add files via upload

---
+++ ArrayClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rohitconsoleapp
+{
+    internal class ArrayClass
+    {
+        public void show()
+        {
+            int[] array = { 20, 30, 40, 50 };
+            Console.WriteLine(array[0]);
+            
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+                foreach (int j in array)
+                {
+                    Console.WriteLine(j);
+
+                }
+                
+           
+            Console.ReadKey();
+        }
+    }
+}
