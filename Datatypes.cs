951c301fd8b360cd74a8f7cf8de9285b38f9c217
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-01 18:49:43 +0530

    Add files via upload

---
+++ Datatypes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rohitconsoleapp
+{
+    internal class Datatypes
+    {
+        public void show()
+        {
+
+            int i = 11;
+            long l = 8478797847;
+            float f = 9.87F;
+            double d = 99.23D;
+            String name = "Rohit";
+            bool result = true;
+            char mh = 'a';
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+            Console.WriteLine(i);
+            Console.WriteLine(l);
+            Console.WriteLine(name);
+            Console.WriteLine(mh);
+            Console.WriteLine(result);
+            Console.WriteLine("Enter Username:");
+            String Username = Console.ReadLine();
+            Console.WriteLine("Username is: " + Username);
+            Console.ReadLine();
+
+        }
+    }
+}
