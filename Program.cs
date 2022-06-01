95b8710ea37af31917efd8778cf24eb20d6e6db4
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-01 15:43:49 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rohitconsoleapp
+{ 
+   
+     class Program
+    {
+        private string model = "BMW";
+
+        static void Main(string[] args)
+        {
+
+        Program prm=new Program();
+            Console.WriteLine(prm.model);
+            Console.ReadKey();
+        }
+        
+    }
+}
