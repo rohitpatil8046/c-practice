b9e5983cc089a88275e1ba7e14610eeb71766d40
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-02 16:15:11 +0530

    Add files via upload

---
+++ Classobject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rohitconsoleapp
+{
+    internal class Classobject
+    {
+        class Car
+        {
+            string color = "White";
+            
+           
+        }
+    }
+}
