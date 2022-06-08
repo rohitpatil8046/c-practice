0006cead1a591137113293ac07e51f044b91765b
rohitpatil8046 <106608815+rohitpatil8046@users.noreply.github.com>
2022-06-08 18:03:12 +0530

    Add files via upload

---
+++ Delegates.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    // Delegate Definition  
+    public delegate int operation(int x, int y);
+
+    class Program
+    {
+        // Method that is passes as an Argument  
+        // It has same signature as Delegates   
+        static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            // Delegate instantiation  
+            operation obj = new operation(Program.Addition);
+
+            // output  
+            Console.WriteLine("Addition is={0}", obj(23, 27));
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
